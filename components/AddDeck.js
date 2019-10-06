77d312d6db2b214904afca9ba4cfe8e1a306ca02
lmidy <lynette.midy@gmail.com>
2019-10-06 04:27:03 -0500

    Added Quiz and Score, modified styling

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,114 +1,84 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
-import CustomButton from './CustomButton';
-import { gray, green, white, textGray } from '../utils/colors';
-import { connect } from 'react-redux';
-import { addDeck } from '../actions';
-import { saveDeck } from '../utils/api';
+import React from 'react'
+import { Text, TextInput, KeyboardAvoidingView, TouchableOpacity, StyleSheet, Alert } from 'react-native'
+import { addDeck } from '../actions'
+import { saveDeck } from '../utils/api'
+import { connect } from 'react-redux'
+import { white, black, gray, blue } from '../utils/colors'
 import { generateUID } from '../utils/helpers'
-import { StackActions, NavigationActions } from 'react-navigation';
+import CustomButton from './CustomButton';
 
-export class AddDeck extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-        text: ''
+
+class AddDeck extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            nameOfDeck: ''
+        }
+    }
+
+    submit = () => {
+        const { nameOfDeck } = this.state
+        if(nameOfDeck === ''){
+            alert('You forgot to enter a Deck Name')
+            return
+        }
+        const deckId = generateUID()
+        const title = nameOfDeck
+        const newDeck = {
+            title: nameOfDeck.trim(), //remove whitespace
+            questions: [] // creating an empty deck
+        }
+        this.props.dispatch(addDeck(deckId, newDeck))
+        this.setState({nameOfDeck: ''})
+        this.toDeck(deckId, title) // navigate to newly created deck
+        saveDeck(deckId, newDeck) //save deck in asyncstorage
+    };
+
+    toDeck = (id, title) => {
+        this.props.navigation.navigate('Deck', {deckId: id, deckName: title})
+    };
+
+    render(){
+        const { nameOfDeck } = this.state
+
+        return(
+            <KeyboardAvoidingView behavior='padding' style={styles.container}>
+                <Text style={styles.title} >Enter your Deck Name</Text>
+                <TextInput
+                 value={nameOfDeck}
+                 style={styles.TextInputStyle}
+                 placeholder="Enter Deck Name here"
+                 onChangeText={(nameOfDeck) => this.setState({nameOfDeck})}
+                />
+                <CustomButton
+                  onPress={this.submit}>
+                    <Text>Create Deck</Text>
+                </CustomButton>
+            </KeyboardAvoidingView>
+        )
     }
 }
 
-  handleChange = text => {
-    this.setState({ text });
-  };
-
-  handleSubmit = () => {
-    const { nameOfDeck } = this.state
-    if(nameOfDeck === ''){
-        alert('Give this deck a name senpai!')
-        return
-    }
-    const deckId = generateUID()
-    const title = nameOfDeck
-    const newDeck = {
-        title: nameOfDeck.trim(), //remove whitespace
-        questions: [] // creating an empty deck
-    }
-    this.props.dispatch(addDeck(deckId, newDeck))
-    this.setState({nameOfDeck: ''})
-    this.toDeck(deckId, title) // navigate to newly created deck
-    saveDeck(deckId, newDeck) //save deck in asyncstorage
-
-    const resetAction = StackActions.reset({
-      index: 1,
-      actions: [
-        NavigationActions.navigate({ routeName: 'H' }),
-        NavigationActions.navigate({
-          routeName: 'DeckDetails',
-          params: { title: text }
-        })
-      ]
-    });
-    navigation.dispatch(resetAction);
-
-    this.setState(() => ({ text: '' }));
-  };
-  render() {
-    const { text }= this.state;
-    return (
-      <View style={styles.container}>
-        <View style={{ height: 60 }} />
-        <View style={styles.block}>
-          <Text style={styles.title}>What is the title of your new deck?</Text>
-        </View>
-        <View style={[styles.block]}>
-          <TextInput
-            style={styles.input}
-            value={this.state.text}
-            onChangeText={this.handleChange}
-            placeholder="Deck Name"
-            autoFocus={true}
-            returnKeyType="done"
-            onSubmitEditing={this.handleSubmit}
-          />
-        </View>
-        <CustomButton
-          btnStyle={{ backgroundColor: green, borderColor: white }}
-          onPress={this.handleSubmit}
-          disabled={this.state.text === ''}
-        >
-          Create Deck
-        </CustomButton>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingBottom: 16,
-    backgroundColor: gray
-  },
-  block: {
-    marginBottom: 20
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 32
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: textGray,
-    backgroundColor: white,
-    paddingLeft: 10,
-    paddingRight: 10,
-    borderRadius: 5,
-    fontSize: 20,
-    height: 40,
-    marginBottom: 20
-  }
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 10
+    },
+    title: {
+      textAlign: 'center',
+      fontSize: 16
+    },
+    TextInputStyle:{
+      textAlign: 'center',
+      height: 40,
+      width: '100%',
+      borderWidth: 1, 
+      borderColor: blue,
+      borderRadius: 10
+    },
+})
 
-export default connect()(AddDeck);
+export default connect()(AddDeck)
--- components/Quiz.js
+++ components/Quiz.js
@@ -1,271 +1,102 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
-import CustomButton from './CustomButton';
-import { gray, green, red, textGray, darkGray, white } from '../utils/colors';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { black } from '../utils/colors'
+import { clearLocalNotification, setLocalNotification } from '../utils/helpers'
+import { connect } from 'react-redux'
+import Card from './Card'
+import Score from './Score'
 
-const screen = {
-  QUESTION: 'question',
-  ANSWER: 'answer',
-  RESULT: 'result'
-};
-const answer = {
-  CORRECT: 'correct',
-  INCORRECT: 'incorrect'
-};
-const SCREEN_WIDTH = Dimensions.get('window').width;
-
-class Quiz extends Component {
-  state = {
-    show: screen.QUESTION,
-    correct: 0,
-    incorrect: 0,
-    questionCount: this.props.deck.questions.length,
-    answered: Array(this.props.deck.questions.length).fill(0)
-  };
-  handleScroll = () => {
-    this.setState({
-      show: screen.QUESTION
-    });
-  };
-  handleAnswer = (response, page) => {
-    if (response === answer.CORRECT) {
-      this.setState(prevState => ({ correct: prevState.correct + 1 }));
-    } else {
-      this.setState(prevState => ({ incorrect: prevState.incorrect + 1 }));
-    }
-    this.setState(
-      prevState => ({
-        answered: prevState.answered.map((val, idx) => (page === idx ? 1 : val))
-      }),
-      () => {
-        // console.log('this.state.answered', this.state.answered);
-        const { correct, incorrect, questionCount } = this.state;
-
-        if (questionCount === correct + incorrect) {
-          this.setState({ show: screen.RESULT });
-        } else {
-          // this.viewPager.setPage(this.state.page + 1);
-          this.scrollView.scrollTo({ x: (page + 1) * SCREEN_WIDTH });
-          // console.log('(page + 1) * SCREEN_WIDTH', (page + 1) * SCREEN_WIDTH);
-          this.setState(prevState => ({
-            show: screen.QUESTION
-          }));
+class Quiz extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            index: 0,
+            showAnswer: false,
+            correct: 0,
+            incorrect: 0
         }
+    }
+    componentDidMount(){
+      clearLocalNotification()
+      .then(setLocalNotification)
+    }
+
+    restart = () => {
+        this.setState({
+            index : 0,
+            showAnswer: false,
+            correct : 0,
+            incorrect : 0,
+    })
+
+    handleAnswer = (result) => {
+        this.setState((state) => ({
+            index : state.index + 1,
+            correct : result === 'correct' ? state.correct + 1 : state.correct,
+            incorrect : result === 'incorrect' ? state.incorrect + 1 : state.incorrect,
+            showAnswer: false
+        }))
+    }
+
+    handleToggle = () => {
+        this.setState((prevState) => ({
+            showAnswer: !prevState.showAnswer
+        }))
+    }
+
+    render(){
+        const { index, showAnswer, correct, incorrect } = this.state
+        const { deckId, deck } = this.props
+        const showCard = index < deck.questions.length ? true : false
+
+        return(
+            <View style={styles.center}>
+                <Text style={styles.count}>{ showCard ? index + 1 : index }/{ deck.questions.length }</Text>
+                {showCard ?
+                    <Card
+                     deck={deck}
+                     flip={this.handleToggle}
+                     index={index}
+                     showAnswer={showAnswer}
+                     answer={this.handleAnswer}
+                    />
+                :
+                    <Score
+                     deck={deck}
+                     deckId={deckId}
+                     restart={this.restart}
+                     correct={correct}
+                     incorrect={incorrect}
+                    />
+                }
+            </View>
+        )
+        
       }
-    );
-  };
-  handleReset = () => {
-    this.setState(prevState => ({
-      show: screen.QUESTION,
-      correct: 0,
-      incorrect: 0,
-      answered: Array(prevState.questionCount).fill(0)
-    }));
-  };
-  render() {
-    const { questions } = this.props.deck;
-    const { show } = this.state;
+}
 
-    if (questions.length === 0) {
-      return (
-        <View style={styles.pageStyle}>
-          <View style={styles.block}>
-            <Text style={[styles.count, { textAlign: 'center' }]}>
-              You cannot take a quiz because there are no cards in the deck.
-            </Text>
-            <Text style={[styles.count, { textAlign: 'center' }]}>
-              Please add some cards and try again.
-            </Text>
-          </View>
-        </View>
-      );
+function mapStateToProps (state, { navigation }) {
+    const { deckId } = navigation.state.params
+    return {
+        deckId,
+        deck: state[deckId]
     }
-
-    if (this.state.show === screen.RESULT) {
-      const { correct, questionCount } = this.state;
-      const percent = ((correct / questionCount) * 100).toFixed(0);
-      const resultStyle =
-        percent >= 70 ? styles.resultTextGood : styles.resultTextBad;
-
-      return (
-        <View style={styles.pageStyle}>
-          <View style={styles.block}>
-            <Text style={[styles.count, { textAlign: 'center' }]}>
-              Quiz Complete!
-            </Text>
-            <Text style={resultStyle}>
-              {correct} / {questionCount} correct
-            </Text>
-          </View>
-          <View style={styles.block}>
-            <Text style={[styles.count, { textAlign: 'center' }]}>
-              Percentage correct
-            </Text>
-            <Text style={resultStyle}>{percent}%</Text>
-          </View>
-          <View>
-            <CustomButton
-              btnStyle={{ backgroundColor: green, borderColor: white }}
-              onPress={this.handleReset}
-            >
-              Restart Quiz
-            </CustomButton>
-            <CustomButton
-              btnStyle={{ backgroundColor: gray, borderColor: textGray }}
-              txtStyle={{ color: textGray }}
-              onPress={() => {
-                this.handleReset();
-                this.props.navigation.goBack();
-              }}
-            >
-              Back To Deck
-            </CustomButton>
-            <CustomButton
-              btnStyle={{ backgroundColor: gray, borderColor: textGray }}
-              txtStyle={{ color: textGray }}
-              onPress={() => {
-                this.handleReset();
-                this.props.navigation.navigate('Home');
-              }}
-            >
-              Home
-            </CustomButton>
-          </View>
-        </View>
-      );
-    }
-
-    return (
-      <ScrollView
-        style={styles.container}
-        pagingEnabled={true}
-        horizontal={true}
-        onMomentumScrollBegin={this.handleScroll}
-        ref={scrollView => {
-          this.scrollView = scrollView;
-        }}
-      >
-        {questions.map((question, idx) => (
-          <View style={styles.pageStyle} key={idx}>
-            <View style={styles.block}>
-              <Text style={styles.count}>
-                {idx + 1} / {questions.length}
-              </Text>
-            </View>
-            <View style={[styles.block, styles.questionContainer]}>
-              <Text style={styles.questionText}>
-                {show === screen.QUESTION ? 'Question' : 'Answer'}
-              </Text>
-              <View style={styles.questionWrapper}>
-                <Text style={styles.title}>
-                  {show === screen.QUESTION
-                    ? question.question
-                    : question.answer}
-                </Text>
-              </View>
-            </View>
-            {show === screen.QUESTION ? (
-              <CustomButton
-                txtStyle={{ color: red }}
-                onPress={() => this.setState({ show: screen.ANSWER })}
-              >
-                Show Answer
-              </CustomButton>
-            ) : (
-              <CustomButton
-                txtStyle={{ color: red }}
-                onPress={() => this.setState({ show: screen.QUESTION })}
-              >
-                Show Question
-              </CustomButton>
-            )}
-            <View>
-              <CustomButton
-                btnStyle={{ backgroundColor: green, borderColor: white }}
-                onPress={() => this.handleAnswer(answer.CORRECT, idx)}
-                disabled={this.state.answered[idx] === 1}
-              >
-                Correct
-              </CustomButton>
-              <CustomButton
-                btnStyle={{ backgroundColor: red, borderColor: white }}
-                onPress={() => this.handleAnswer(answer.INCORRECT, idx)}
-                disabled={this.state.answered[idx] === 1}
-              >
-                Incorrect
-              </CustomButton>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  pageStyle: {
+  center: {
     flex: 1,
-    paddingTop: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingBottom: 16,
-    backgroundColor: gray,
-    justifyContent: 'space-around',
-    width: SCREEN_WIDTH
-  },
-  block: {
-    marginBottom: 20
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   count: {
-    fontSize: 24
-  },
-  title: {
-    fontSize: 32,
-    textAlign: 'center'
-  },
-  questionContainer: {
-    borderWidth: 1,
-    borderColor: darkGray,
-    backgroundColor: white,
-    borderRadius: 5,
-    paddingTop: 20,
-    paddingBottom: 20,
-    paddingLeft: 16,
-    paddingRight: 16,
-    flexGrow: 1
-  },
-  questionWrapper: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  questionText: {
-    textDecorationLine: 'underline',
-    textAlign: 'center',
-    fontSize: 20
-  },
-  resultTextGood: {
-    color: green,
-    fontSize: 46,
-    textAlign: 'center'
-  },
-  resultTextBad: {
-    color: red,
-    fontSize: 46,
-    textAlign: 'center'
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    marginLeft: 10,
+    color: black,
+    fontWeight: 'bold',
+    fontSize: 18
   }
-});
+})
 
-const mapStateToProps = state => ({ decks: state });
-
-const mapDispatchToProps = {};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Quiz);
+export default connect(mapStateToProps)(Quiz)
